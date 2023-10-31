ae14707f8e34eacf9fe7efcb38438520a39e1433
Emir Risyad <emirrsyd@gmail.com>
2023-10-31 22:56:42 +0100

    orders route fix

---
+++ app/api/[storeId]/orders/route.ts
@@ -0,0 +1,87 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { productId, phone, isPaid, status } = body;
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!phone) {
+      return new NextResponse('Phone is required', { status: 400 });
+    }
+
+    if (!productId) {
+      return new NextResponse('Product id is required', { status: 400 });
+    }
+
+    if (!status) {
+      return new NextResponse('Status is required', { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse('Store id is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const order = await prismadb.order.create({
+      data: {
+        //! DATABASE RELATION ERROR
+        productId,
+        phone,
+        status,
+        isPaid,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(order);
+  } catch (error) {
+    console.log('[ORDERS_POST]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse('Store id is required', { status: 400 });
+    }
+
+    const orders = await prismadb.order.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return NextResponse.json(orders);
+  } catch (error) {
+    console.log('[ORDERS_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
